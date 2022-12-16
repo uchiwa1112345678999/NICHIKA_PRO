2d35642f8fdaba7d56e9211aeab0a684fa83c690
uchiwa <75378129+uchiwa1112345678999@users.noreply.github.com>
2022-12-16 23:29:37 +0900

    Update nichika.py

--- nichika.py
+++ nichika.py
@@ -1,5 +1,5 @@
 import cv2
-img = cv2.imread('C:\\Users\\atsuya\\Desktop\\uchiwa_python\\IMG_0231.jpg' , cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('image' , cv2.IMREAD_GRAYSCALE)
 ret,gpt = cv2.threshold(img , 80 , 210, cv2.THRESH_BINARY)
 cv2.imshow('KOHARU' , gpt)
 cv2.waitKey(0)
